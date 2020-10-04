7d55a0b006e66d64266db80c6f0b2f365bde1fa0
Valeria <v.ahmetzhanova@gmail.com>
2020-10-04 17:12:37 +0300

    improve task3 solution

--- task3/myScript.js
+++ task3/myScript.js
@@ -1,8 +1,10 @@
-window.onload = () => task3();
+let text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
 
-function task3() {
-    let match = document.body.textContent.match(/\b\w*ing\b/ig);
-    for (let i = 0; i < match.length; i++) {
-        document.body.innerHTML = document.body.innerHTML.replace(match[i], "<span class='task3--word-highlighted'>" + match[i] + "</span>");
+for (let i = 0; i < text.length; i++) {
+    let match = text[i].textContent.match(/\b\w*ing\b/ig);
+    if (match !== null) {
+        for (let j = 0; j < match.length; j++) {
+            text[i].innerHTML = text[i].innerHTML.replace(match[j], "<span class='task3--word-highlighted'>" + match[j] + "</span>");
+        }
     }
 }
