a37377a1e37e34db215d596b357cf18aef11da51
Valeria <v.ahmetzhanova@gmail.com>
2020-10-04 23:42:18 +0300

    minor fix

--- task3/myScript.js
+++ task3/myScript.js
@@ -1,4 +1,4 @@
-let text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
+var text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
 
 for (let i = 0; i < text.length; i++) {
     let match = text[i].textContent.match(/\b\w*ing\b/ig);
